469277ecde971d9cd2a4f1710b92b4af678b1fcd
wmz7year <ydswcy513@gmail.com>
2015-11-19 15:01:08 +0800

    动态增加或者移除服务器测试成功

--- src/test/java/com/wmz7year/thrift/pool/DynamicServersTest.java
+++ src/test/java/com/wmz7year/thrift/pool/DynamicServersTest.java
@@ -16,19 +16,63 @@
 
 package com.wmz7year.thrift.pool;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.wmz7year.thrift.pool.config.ThriftConnectionPoolConfig;
+import com.wmz7year.thrift.pool.config.ThriftConnectionPoolConfig.TProtocolType;
+import com.wmz7year.thrift.pool.config.ThriftServerInfo;
+import com.wmz7year.thrift.pool.connection.ThriftConnection;
+import com.wmz7year.thrift.pool.example.Example;
+import com.wmz7year.thrift.pool.example.Example.Client;
+
+/*
+ * 测试动态添加或者删除服务器列表功能
+ */
 public class DynamicServersTest extends BasicAbstractTest {
+
+	private List<ThriftServerInfo> servers;
 
 	@Override
 	protected void beforeTest() throws Exception {
-		System.out.println("beforeTest");
+		this.servers = startServers(2);
+	}
+
+	public void testDynamicServer() throws Throwable {
+		ThriftConnectionPoolConfig config = new ThriftConnectionPoolConfig();
+		config.setConnectTimeout(3000);
+		config.setThriftProtocol(TProtocolType.BINARY);
+		config.setClientClass(Example.Client.class);
+		for (ThriftServerInfo thriftServerInfo : servers) {
+			config.addThriftServer(thriftServerInfo.getHost(), thriftServerInfo.getPort());
+		}
+		config.setMaxConnectionPerServer(2);
+		config.setMinConnectionPerServer(1);
+		config.setIdleMaxAge(2, TimeUnit.SECONDS);
+		config.setMaxConnectionAge(2);
+		config.setLazyInit(false);
+		ThriftConnectionPool<Example.Client> pool = new ThriftConnectionPool<Example.Client>(config);
+
+		int testCount = (servers.size() + 1) * config.getMaxConnectionPerServer() * 10;
+		ThriftServerInfo newServer = startServer();
+		for (int i = 0; i < testCount; i++) {
+			if (i % 3 == 0) {
+				logger.info("添加新服务器");
+				pool.addThriftServer(newServer);
+			} else if (i % 7 == 0) {
+				logger.info("移除服务器");
+				pool.removeThriftServer(newServer);
+			}
+			ThriftConnection<Client> connection = pool.getConnection();
+			connection.getClient().ping();
+			connection.close();
+		}
+
+		pool.close();
 	}
 
 	@Override
 	protected void afterTest() throws Exception {
-		System.out.println("afterTest");
-	}
-
-	public void test() {
-		System.out.println("sadsadasd");
+		// ignore
 	}
 }
